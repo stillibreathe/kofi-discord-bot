ed58106c3c8682f9bfa04ab64b52a13a60a45624
stillibreathe <95fils@gmail.com>
2026-04-28 02:14:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const app = express();
 
 app.use(express.json());
 
+// اختبار السيرفر
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+// Webhook
 app.post("/webhook", (req, res) => {
   console.log("🔥 Event received:");
-  console.log(req.body);
-  res.send("OK");
+  console.log(JSON.stringify(req.body, null, 2));
+
+  res.status(200).send("OK");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Running on", PORT);
+  console.log("🚀 Running on port", PORT);
 });
